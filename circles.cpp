5468939a7bc2c0b45c37bd768d6d2d1f37d34845
Napwob <gorno-altaisk0@mail.ru>
2019-03-16 13:57:12 +0300

    Too Many Times(reading many times)

--- circles.cpp
+++ circles.cpp
@@ -20,34 +20,40 @@ int readin(char *h,int& i,char el){
 
 int main()
 {
-    int x,y,r,x2=0,y2=0,r2=0;
-    int i=1;
+    int x,y,r,x2=0,y2=0,r2=0,k=0,sh=0;
+    int i=0;
     double s;
-    char h[100],f1[10],f2[10];
+    char h[100],f[10];
 
-    printf("VVedite koordinati po tipu 'circle(x,y,r),circle(x2,y2,r2)'\n");//(10,10,10)
+    while(sh==0){
+    i=0;
+    printf("VVedite koordinati po tipu 'circle(x,y,r)'\n"); //,circle(x2,y2,r2)'\n");//(10,10,10)
     gets(h);
-
-
+    while(h[i]!='('){
+        f[i]=h[i];
+        i++;
+    }
+    i++;
 
     x=readin(h,i,',');
     y=readin(h,i,',');
     r=readin(h,i,')');
 
-    i+=2;
+    //i+=2;
 
-    x2=readin(h,i,',');
-    y2=readin(h,i,',');
-    r2=readin(h,i,')');
+    //x2=readin(h,i,',');
+    //y2=readin(h,i,',');
+    //r2=readin(h,i,')');
 
-    //printf("%d - %d - X\n%d - %d - Y\n%d - %d - R\n",x,x2,y,y2,r,r2);
-    int stx,sty;
-    double gi;
-    stx=x2-x;
-    sty=y2-y;
-    gi=sqrt((stx*stx)+(sty*sty));
-    printf("%d - stx\n%d - sty\n%f - gi\n%d - r+r2\n",stx,sty,gi,(r+r2));
-    if (gi<=(r+r2)) printf("\nYes"); else printf("\nNo");
+    printf("%d - X\n%d - Y\n%d - R\n",x,y,r);
+    //int stx,sty;
+    //double gi;
+    //stx=x2-x;
+    //sty=y2-y;
+    //gi=sqrt((stx*stx)+(sty*sty));
+    //printf("%d - stx\n%d - sty\n%f - gi\n%d - r+r2\n",stx,sty,gi,(r+r2));
+    //if (gi<=(r+r2)) printf("\nYes"); else printf("\nNo");
     s=r*r*M_PI;
-    //printf("\n%.14f",s);
+    printf("\n%.14f\n",s);
+    }
     }
