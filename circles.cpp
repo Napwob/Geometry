463e9f6aba1bb4c0873f970daa582049a4ba2b0f
Napwob <gorno-altaisk0@mail.ru>
2019-03-16 15:07:42 +0300

    Some Errors(proverka na pravilnost napisania)

--- circles.cpp
+++ circles.cpp
@@ -21,47 +21,54 @@ int readin(char *h,int& i,char el){
 
 int main()
 {
-    int x,y,r,x2=0,y2=0,r2=0,k=0,sh=0;
+    int x[100],y[100],r[100],en=0,number=0,error=0;
     int i=0;
     double s;
-    char h[100],f[10];
+    char h[100],f[6],f1[]={"circle"};;
 
-    while(sh==0){
+
+
+    while(en==0){
     i=0;
-    printf("VVedite koordinati po tipu 'circle(x,y,r)'\n"); //,circle(x2,y2,r2)'\n");//(10,10,10)
+    error=0;
+    printf("%d. ",number);
     gets(h);
+
+
     while(h[i]!='('){
         f[i]=h[i];
+        if(f[i]!=f1[i]) error++;
         i++;
     }
+
+    if(error==0)
+    {
     i++;
 
-    x=readin(h,i,',');
-    y=readin(h,i,',');
-    r=readin(h,i,')');
+    x[number]=readin(h,i,',');
+    y[number]=readin(h,i,',');
+    r[number]=readin(h,i,')');
 
-    //i+=2;
 
-    //x2=readin(h,i,',');
-    //y2=readin(h,i,',');
-    //r2=readin(h,i,')');
-
-    printf("%d - X\n%d - Y\n%d - R\n",x,y,r);
-    //int stx,sty;
-    //double gi;
-    //stx=x2-x;
-    //sty=y2-y;
-    //gi=sqrt((stx*stx)+(sty*sty));
-    //printf("%d - stx\n%d - sty\n%f - gi\n%d - r+r2\n",stx,sty,gi,(r+r2));
-    //if (gi<=(r+r2)) printf("\nYes"); else printf("\nNo");
-    s=r*r*M_PI;
-    printf("\n%.14f\n",s);
+    printf("%d - X\n%d - Y\n%d - R\n",x[number],y[number],r[number]);
+    number++;
+    } else {
+        printf("Error: unknown shape ");
+        puts(f);
+        printf("\n");
+        }
+    error=0;
     }
-    int stx,sty;
-    double gi;
-    stx=x2-x;
-    sty=y2-y;
-    gi=sqrt((stx*stx)+(sty*sty));
-    printf("%d - stx\n%d - sty\n%f - gi\n%d - r+r2\n",stx,sty,gi,(r+r2));
-    if (gi<=(r+r2)) printf("\nYes"); else printf("\nNo");
+
+
+
+    //s=r*r*M_PI;
+   // printf("\n%.14f\n",s);
+   // int stx,sty;
+   // double gi;
+   // stx=x2-x;
+   // sty=y2-y;
+   // gi=sqrt((stx*stx)+(sty*sty));
+   // printf("%d - stx\n%d - sty\n%f - gi\n%d - r+r2\n",stx,sty,gi,(r+r2));
+   // if (gi<=(r+r2)) printf("\nYes"); else printf("\nNo");
     }
