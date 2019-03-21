ab7b3143c813d3552bb06532966c50a2b2bd1ac8
Napwob <gorno-altaisk0@mail.ru>
2019-03-21 11:09:22 +0300

    circle(Final Version)

--- circles.cpp
+++ circles.cpp
@@ -1,74 +1,64 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-
-int readin(char *h,int& i,char el){
-    int k=0,coor;
+int readin(char* h, int& i, char el)
+{
+    int k = 0, coor;
     char ch[100];
     memset(ch, 0, sizeof(ch));
-    while(h[i]!=el){
-        ch[k]=h[i];
+    while (h[i] != el) {
+        ch[k] = h[i];
         k++;
         i++;
     }
-    coor=atoi(ch);
+    coor = atoi(ch);
     i++;
     return coor;
 }
 
-
 int main()
 {
-    int x[100],y[100],r[100],en=0,number=0,error=0;
-    int i=0;
+    int x, y, r, en = 0, error = 0, errorcounter = 0;
+    int i;
     double s;
-    char h[100],f[6],f1[]={"circle"};;
+    char h[1000], f1[] = { "circle" };
 
+    while (en == 0) {
+        printf("Please, enter 'circle(x,y,r)'\n");
+        gets(h);
 
-
-    while(en==0){
-    i=0;
-    error=0;
-    printf("%d. ",number);
-    gets(h);
-
-
-    while(h[i]!='('){
-        f[i]=h[i];
-        if(f[i]!=f1[i]) error++;
-        i++;
-    }
-
-    if(error==0)
-    {
-    i++;
-
-    x[number]=readin(h,i,',');
-    y[number]=readin(h,i,',');
-    r[number]=readin(h,i,')');
-
-
-    printf("%d - X\n%d - Y\n%d - R\n",x[number],y[number],r[number]);
-    number++;
-    } else {
-        printf("Error: unknown shape ");
-        puts(f);
-        printf("\n");
+        for (i = 0; i < 6; i++) {
+            if (h[i] != f1[i]) {
+                error++;
+            }
         }
-    error=0;
+        if (error == 0) {
+            i = 7;
+            x = readin(h, i, ',');
+            y = readin(h, i, ',');
+            r = readin(h, i, ')');
+        }
+
+        if (error == 0 && x != 0 && y != 0 && r != 0) {
+            double s, p;
+            s = r * r * M_PI;
+            p = 2 * M_PI * r;
+
+            printf("Type of Figure: ");
+            puts(f1);
+            printf("%d - X\n%d - Y\n%d - R\n", x, y, r);
+            printf("Area = %.4f\n", s);
+            printf("Perimeter= %.4f", p);
+
+            en++;
+        }
+        else {
+            printf("Error: unknown shape or wrong coordinates\n");
+        }
+        error = 0;
     }
+}
 
 
-
-    //s=r*r*M_PI;
-   // printf("\n%.14f\n",s);
-   // int stx,sty;
-   // double gi;
-   // stx=x2-x;
-   // sty=y2-y;
-   // gi=sqrt((stx*stx)+(sty*sty));
-   // printf("%d - stx\n%d - sty\n%f - gi\n%d - r+r2\n",stx,sty,gi,(r+r2));
-   // if (gi<=(r+r2)) printf("\nYes"); else printf("\nNo");
-    }
